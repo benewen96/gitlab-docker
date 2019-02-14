02c9d0bf205df553ca3a1b3131063c5c8b098862
Ben Ewen <benewen@outlook.com>
2019-02-14 16:00:14 +0000

    Update generate-gitlab-rb.sh

--- generate-gitlab-rb.sh
+++ generate-gitlab-rb.sh
@@ -1,3 +1,3 @@
 # $1 = gitlab url
 # $2 = registry external url
-echo -e " external_url '$1'\n gitlab_rails['initial_root_password'] = File.read('/run/secrets/gitlab_root_password')\n registry_external_url '$2'" > gitlab.rb
+printf " external_url '$1'\n gitlab_rails['initial_root_password'] = File.read('/run/secrets/gitlab_root_password')\n registry_external_url '$2'" > gitlab.rb
