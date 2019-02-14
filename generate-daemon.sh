1d0aef9f6f986256ef8ad35c0be6fa8dbe7194c2
Ben Ewen <b4002025@my.shu.ac.uk>
2019-02-14 15:45:58 +0000

    changes

--- generate-daemon.sh
+++ generate-daemon.sh
@@ -1,2 +1,2 @@
 # $1 = registry ip + port: http://gitlab:1234
-echo '{\"insecure-registries\" : [\""$1"\"]}' | sudo tee /etc/docker/daemon.json > /dev/null
+echo "{\"insecure-registries\" : [ \"$1\" ]}" | sudo tee /etc/docker/daemon.json > /dev/null
--- gitlab.rb
+++
@@ -1,3 +0,0 @@
- external_url 'http://192.168.0.19'
- gitlab_rails['initial_root_password'] = File.read('/run/secrets/gitlab_root_password')
- registry_external_url 'http://192.168.0.19:4567'
